49516037b74910b19469a0022cbd471b59b9c98b
Futureppo <luominzhi2005@qq.com>
2025-03-24 21:28:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
+import re
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.platform import AstrBotMessage
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+from astrbot.api.provider import LLMResponse
+from openai.types.chat.chat_completion import ChatCompletion
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
-class MyPlugin(Star):
-    def __init__(self, context: Context):
+@register("keyinterceptor", "Futureppo", "屏蔽获取不到model列表导致key泄露", "1.0.0")
+class keyinterceptor(Star):
+    def __init__(self, context: Context, config: dict):
         super().__init__(context)
+        self.config = config
+        self.IsError_filter = self.config.get('iskeyinterceptor', True)
     
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
-
-    async def terminate(self):
-        '''可选择实现 terminate 函数，当插件被卸载/停用时会调用。'''
+    @filter.on_decorating_result()
+    async def on_decorating_result(self, event: AstrMessageEvent):
+        result = event.get_result()
+        message_str = result.get_plain_text()
+        if self.IsError_filter:
+            if '获取模型列表失败' in message_str:
+                logger.info(message_str)
+                event.stop_event() # 停止事件传递
